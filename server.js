a56083979b81d96bd24f83291451adfd90f1bcd9
Tosking <tosking@duck.com>
2023-10-21 12:19:23 +0500

    added input handlers, node modules and signin method

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const fs = require('fs')
 const path = require('path');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
-const { createHash } = require('crypto');
+const createHash = require('crypto');
+const token = require("./token.js")
+const inputHandler = require("./handleInput.js")
 const app = express()
 const port = 3000
 require('dotenv').config()
@@ -26,9 +28,9 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-app.post("/auth/register", (req, res) => {
+app.post("/auth/signup", (req, res) => {
   console.log(req.body)
-  if(req.body.login == null || req.body.password == null || !req.body.email.match("^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$")){
+  if(inputHandler.signupHandler(req)){
     res.sendStatus(400)
     return
   }
@@ -36,8 +38,8 @@ app.post("/auth/register", (req, res) => {
   const result = pg.query(`SELECT id FROM users WHERE password = '${pass}' AND email = '${req.body.email}'`, (result) => {
     if(result.rows[0].id == undefined){
       pg.query(`INSERT INTO users (login, email, password) VALUES ('${req.body.login}', '${req.body.email}', '${pass}') RETURNING id, login;`, (err, result) => {
-        const randomNumber=createHash('sha256').update(req.body.password + req.body.email).digest('hex')
-        res.cookie('cookie',randomNumber, { maxAge: 900000, httpOnly: true });
+        const userToken = token.generateAccessToken(result.rows[0].login)
+        res.cookie('token',userToken, { maxAge: 900000, httpOnly: true });
         res.cookie('id', result.rows[0].id)
         res.cookie('login', result.rows[0].login)
         res.sendStatus(200)
@@ -46,6 +48,28 @@ app.post("/auth/register", (req, res) => {
     else {
       res.status(400).send("Account with that email already exists")
     }
+  })
+})
+
+app.post("/auth/signin", (req, res) => {
+  const pass = createHash('sha256').update(req.body.password).digest('hex')
+  const result = pg.query(`SELECT id, login FROM users WHERE password = '${pass}' AND email = '${req.body.email}'`, (err, result) => {
+      if(result.rows.length == 0){
+          res.status(400).send("Email или пароль введены неверно")
+          return
+      }
+      else if(inputHandler.signinHandler(req)){
+        res.status(400).send("Email или пароль введены неверно")
+        return
+      }
+      else {
+          const userToken = token.generateAccessToken(result.rows[0].login)
+          res.cookie('cookie',userToken, { maxAge: 90000, httpOnly: true });
+          res.cookie('id', result.rows[0].id)
+          res.cookie('login', result.rows[0].login)
+          res.sendStatus(200)
+          return
+      }
   })
 })
 
