54264b1dce89e5f723c3cef888cb91d9c4451a53
ZkunCry <aksentev.03@mail.ru>
2023-11-29 00:40:45 +0500

    added checked refresh token

--- routerPrivate.js
+++ routerPrivate.js
@@ -9,10 +9,9 @@ routerPrivate.use((req, res, next) => {
     accessToken = token.authenticateAccessToken(req);
   } catch (TokenExpiredError) {
     res.status(401).send("Token has expired");
-    return
+    return;
   }
-  if (!accessToken) {
-    console.log(accessToken)
+  if (!accessToken && !req.cookies["refreshToken"]) {
     res.status(401).send("No token has been provided");
   } else {
     req.body.tokenID = accessToken;
@@ -43,7 +42,7 @@ routerPrivate.post("/task/create", (req, res) => {
         console.log(err);
         res.status(500).send("Error when inserting tasks into database");
       } else {
-        task.id = result.rows[0].id
+        task.id = result.rows[0].id;
         res.status(200).json(task);
       }
     }
@@ -51,18 +50,20 @@ routerPrivate.post("/task/create", (req, res) => {
 });
 
 routerPrivate.post("/task/delete", (req, res) => {
-    pg.query(`DELETE FROM task WHERE id=${req.body.id} AND userid=${req.body.tokenID.id} RETURNING id`, (err, result) => {
-        if(!result.rows[0]){
-            res.status(400).send("Non-existent task")
-        }
-        else{
-            res.status(200).send(result.rows[0].id)
-        }
-    })
-})
+  pg.query(
+    `DELETE FROM task WHERE id=${req.body.id} AND userid=${req.body.tokenID.id} RETURNING id`,
+    (err, result) => {
+      if (!result.rows[0]) {
+        res.status(400).send("Non-existent task");
+      } else {
+        res.status(200).send(result.rows[0].id);
+      }
+    }
+  );
+});
 
 routerPrivate.post("/category/create", (req, res) => {
-    const task = inputHandler.taskInputHandler(req);
+  const task = inputHandler.taskInputHandler(req);
   pg.query(
     `INSERT INTO category (userid, title, description) VALUES ('${req.body.tokenID.id}', '${task.title}', '${task.description}') RETURNING id`,
     (err, result) => {
@@ -70,23 +71,25 @@ routerPrivate.post("/category/create", (req, res) => {
         console.log(err);
         res.status(500).send("Error when inserting category into database");
       } else {
-        task.id = result.rows[0].id
+        task.id = result.rows[0].id;
         res.status(200).json(task);
       }
     }
   );
-})
+});
 
 routerPrivate.post("/category/delete", (req, res) => {
-    pg.query(`DELETE FROM category WHERE id=${req.body.id} AND userid=${req.body.tokenID.id} RETURNING id`, (err, result) => {
-        if(!result.rows[0]){
-            res.status(400).send("Non-existent category")
-        }
-        else{
-            res.status(200).send(result.rows[0].id)
-        }
-    })
-})
+  pg.query(
+    `DELETE FROM category WHERE id=${req.body.id} AND userid=${req.body.tokenID.id} RETURNING id`,
+    (err, result) => {
+      if (!result.rows[0]) {
+        res.status(400).send("Non-existent category");
+      } else {
+        res.status(200).send(result.rows[0].id);
+      }
+    }
+  );
+});
 
 module.exports = {
   routerPrivate,
